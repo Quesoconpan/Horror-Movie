eb342b8a9aa44579cecce08df61bf697408c66f4
Quesoconpan <88353587+Quesoconpan@users.noreply.github.com>
2021-10-25 19:10:45 -0500

    Add files via upload

---
+++ StabKiller.js
@@ -0,0 +1,13 @@
+class StabKiller extends baseClass {
+
+constructor(x,y,width,height) {
+super(x,y,width,height);
+this.image = loadImage("sprites/Ghostface (The Killer)/Slasher01.png");
+this.image2 = loadImage("sprites/Ghostface (The Killer)/Slasher03.png");
+
+}
+display(){
+    super.display();
+}
+
+}
